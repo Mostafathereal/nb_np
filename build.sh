25dee7736f0b8c6698866f1066ada46b08e8ba4a
Mostafa <reachmostafa.m@gmail.com>
2026-01-31 18:55:05 -0500

    dont automatically run main executable after build

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ cd build
 cmake ..
 cmake --build .
 
-if [ $? -eq 0 ]; then
-    echo -e "\n--- Running Program ---\n"
-    ./main
-else
-    echo -e "\nBuild Failed!"
-fi
+# if [ $? -eq 0 ]; then
+#     echo -e "\n--- Running Program ---\n"
+#     ./main
+# else
+#     echo -e "\nBuild Failed!"
+# fi
